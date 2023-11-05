c3b5374bbd951ee8729c3a0beb1d1fc9f2d92f07
Jackson Makinda <me@makindajackson.com>
2023-11-05 22:35:24 +0300

    :8ball: renaming tings...

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangogirlsweb.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangogirls.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
